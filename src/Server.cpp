f7b4e5eba9f8aa50f1cc95f507d91c77641fba40
Dario Didi <dididario.dd2@gmail.com>
2025-08-19 17:55:20 +0300

    feat: SET with expiry

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,9 +1,11 @@
 #include <arpa/inet.h>
-#include <cmath>
-#include <cstddef>
-#include <cstdint>
-#include <cstdlib>
+#include <chrono>
+// #include <cmath>
+// #include <cstddef>
+// #include <cstdint>
+// #include <cstdlib>
 #include <cstring>
+#include <ctime>
 #include <iostream>
 #include <netdb.h>
 #include <netinet/in.h>
@@ -56,40 +58,73 @@ std::vector<std::vector<std::string>> getCommand(std::string);
 std::vector<std::vector<std::string>> readCommandsFromRespArray(std::string);
 int writeResponse(int fd, std::string buf);
 std::string getBulkString(std::string);
-// void handleSet(std::vector<std::string>, std::unordered_map<std::string, std::string>);
-// int handleGet(std::string, std::unordered_map<std::string, std::string>, int);
 
-// void handleSet(std::vector<std::string> singleCommand, std::unordered_map<std::string, std::string> redisDb)
-//{
-//     auto key = singleCommand[1];
-//     auto val = singleCommand[2];
-//     redisDb[key] = val;
-// }
+struct dbValue
+{
+        std::string val;
+        // time_t expiryTime;
+        std::chrono::system_clock::time_point expiryTime;
+        // std::time_t expiryTime;
+        //   std::chrono::duration<std::chrono::milliseconds> expiryTime;
+        //    unsigned long expiryTime;
+        bool hasExpiry;
+};
 
 class MainDB
 {
-        std::unordered_map<std::string, std::string> htmap;
+        std::unordered_map<std::string, dbValue> htmap;
 
     public:
-        void set(std::string key, std::string value)
+        void set(std::vector<std::string> singleCommand, std::string value)
         {
-            htmap[key] = value;
+            dbValue newVal;
+
+            newVal.val = value;
+            newVal.hasExpiry = false;
+            if (singleCommand.size() > 4)
+            {
+                auto timeType = singleCommand[3];
+                std::transform(timeType.begin(), timeType.end(), timeType.begin(),
+                               [](unsigned char c)
+                               { return std::tolower(c); });
+                if (timeType == "px")
+                {
+                    newVal.hasExpiry = true;
+                    auto expTime = stoi(singleCommand[4]);
+                    auto msTime = std::chrono::milliseconds(expTime);
+                    auto currTime = std::chrono::system_clock::now();
+                    auto useTime = currTime + msTime;
+                    newVal.expiryTime = useTime;
+                    std::cerr << "\n\nadding expiry of: " << msTime.count()
+                              << "to current time: " << currTime.time_since_epoch().count() << "\n\n"
+                              << " expiry set to " << useTime.time_since_epoch().count() << "\n\n";
+                }
+            }
+            htmap[singleCommand[1]] = newVal;
         }
 
         const int get(const std::string key, int fd)
         {
             for (const auto &[key, value] : htmap)
-            {
-                std::cerr << "IN KV LOOP\n";
-                std::cerr << " found key: " << key << "\nval: " << value << "\n";
-            }
-            auto res = htmap.find(key);
+                std::cerr << "IN KV LOOP\n"
+                          << " found key: " << key << "\nval: " << value.val << "\n";
+
             std::cerr << "map size: " << htmap.size() << "\n";
-            if (res != htmap.end())
+
+            if (htmap.find(key) != htmap.end())
             {
-                auto val = htmap[key];
-                std::cerr << "Writing GET val: " << val << "\n";
-                return writeResponse(fd, getBulkString(val));
+
+                auto curr_val = htmap[key];
+                std::cerr << "Writing GET val: " << curr_val.val << "\n"
+                          << "val has expiry?: " << curr_val.hasExpiry << "\n"
+                          << "val  expiry?: " << curr_val.expiryTime.time_since_epoch().count() << "\n";
+                auto currTime = std::chrono::system_clock::now();
+                std::cerr << "currTIme" << currTime.time_since_epoch().count() << "\n";
+
+                if (!curr_val.hasExpiry ||
+                    (curr_val.hasExpiry &&
+                     curr_val.expiryTime > currTime))
+                    return writeResponse(fd, getBulkString(curr_val.val));
             }
             std::cerr << "Failed GET for: " << key << "\n";
             writeResponse(fd, RESP_NULL);
@@ -349,7 +384,7 @@ int main(int argc, char **argv)
                             break;
 
                         case Set:
-                            mainDb.set(singleCommand[1], singleCommand[2]);
+                            mainDb.set(singleCommand, singleCommand[2]);
                             numWritten = writeResponse(sd, RESP_OK);
                             break;
 
